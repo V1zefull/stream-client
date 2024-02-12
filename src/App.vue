9229f191a825bf911dfb69ff79797e5757592e25
Egor Borovlev <eab@promtexklg.ru>
2024-02-12 14:20:47 +0300

    Синхронизация проекта

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <div id="app">
+  <div>
     <navBar/>
+    <homePage />
   </div>
 </template>
 
 <script>
-import navBar from './components/header/navBar.vue'
+
+import homePage from './page/home/homePage.vue'
+
 
 export default {
   name: 'App',
   components: {
-    navBar
+    homePage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/page/home/homePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <navBarVue />
+    <div class="container">
+      Компонент с различными трансляциями
+    </div>
+  </div>
+</template>
+
+<script>
+import navBarVue from "@/components/header/navBar.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    navBarVue,
+  },
+};
+</script>
+
+<style scoped>
+.home{
+  width:100%;
+  height: 963px;
+  background-color: rgb(247, 224, 160);
+}
+
+.container{
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
